f03d3678f9869a158b79fce1c0ca7b55f13ae733
VelkyCajicek <peichl294@gmail.com>
2024-07-09 13:08:49 +0100

    Alkanes, Alkenes, Alkynes added

--- OCGUI.py
+++ OCGUI.py
@@ -1,7 +1,7 @@
 import tkinter as tk 
 import turtle
 
-from formula import FormulaConversion
+from formula import mainFormula
 
 class GUI:
     def __init__(self) -> None:
@@ -12,8 +12,6 @@ class GUI:
     def initialize(self):
         self.root.title("Organic Chemistry")
         self.root.geometry("800x800")
-
-        self.Conversion = FormulaConversion()
 
         self.userInputBox = tk.Entry(self.root)
         self.userInputBox.pack()
@@ -27,8 +25,9 @@ class GUI:
 
     def main(self, t : turtle.RawTurtle):
         t.reset() # Resets the canvas
-        compoundData = self.Conversion.mainFormula(self.userInputBox.get())
-        self.DrawAlkanes(t, len(compoundData), compoundData)
+        compoundData, bondPositions = mainFormula(self.userInputBox.get().strip().lower()) # White spaces are very bad
+        self.DrawAlkanesSimple(t, len(compoundData), compoundData, bondPositions)
+        print(compoundData)
 
     def DrawHydrocarbonResidue(self, currentAngle : float, t : turtle.RawTurtle, compoundDataElement : int):
         currentPosX = t.xcor()
@@ -36,8 +35,7 @@ class GUI:
         t.setheading(90 + currentAngle)
         t.forward(30)
         t.left(30 + currentAngle)
-        for i in range(0, compoundDataElement - 1):
-            print(i, t.heading())
+        for _ in range(0, compoundDataElement - 1):
             if(t.heading() == 30.0 + currentAngle / 2): # This doesnt yet work so currentAngle will be set to 0
                 t.forward(30)
                 t.right(60) 
@@ -45,34 +43,36 @@ class GUI:
                 t.forward(30)
                 t.left(60)
         t.penup()
-        t.goto(currentPosX, currentPosY)
+        t.goto(currentPosX, currentPosY) # Resets position
         t.pendown()
 
-    def DrawAlkanes(self, t : turtle.RawTurtle, mainCarbonCount : int, compoundData : list):
+    def DrawAdditionalInfo(self, rotation : int, compoundData : list, i : int, bondPositions : list, t : turtle.RawTurtle):
+        if(compoundData[i] != 0): # These parts just mean that it draws so residue part and then continues drawing the backbone
+            if(type(compoundData[i]) is list):
+                self.DrawHydrocarbonResidue(0, t, compoundData[i][0])
+                self.DrawHydrocarbonResidue(180, t, compoundData[i][1])
+            else:
+                self.DrawHydrocarbonResidue(0, t, compoundData[i])
+            t.setheading(rotation)
+        if(bondPositions[i] != 0):
+            if(bondPositions[i] == 2):
+                t.pencolor("red")
+            if(bondPositions[i] == 3):
+                t.pencolor("green")
+
+    def DrawAlkanesSimple(self, t : turtle.RawTurtle, mainCarbonCount : int, compoundData : list, bondPositions : list):
         t.left(30)
         for i in range(0, mainCarbonCount - 1): # -1 since each end point is a carbon
             if(t.heading() == 30.0):
-                if(compoundData[i] != 0): # These parts just mean that it draws so residue part and then continues drawing the backbone
-                    if(type(compoundData[i]) is list):
-                        print("Works")
-                        self.DrawHydrocarbonResidue(0, t, compoundData[i][0])
-                        self.DrawHydrocarbonResidue(180, t, compoundData[i][1])
-                    else:
-                        self.DrawHydrocarbonResidue(0, t, compoundData[i])
-                    t.setheading(30)
+                self.DrawAdditionalInfo(30, compoundData, i, bondPositions, t)
                 t.forward(30)
                 t.right(60)
+                t.pencolor("black")
             else:
-                if(compoundData[i] != 0):         
-                    if(type(compoundData[i]) is list):
-                        print("Works")
-                        self.DrawHydrocarbonResidue(0, t, compoundData[i][0])
-                        self.DrawHydrocarbonResidue(180, t, compoundData[i][1])
-                    else:
-                        self.DrawHydrocarbonResidue(0, t, compoundData[i])
-                    t.setheading(330)
+                self.DrawAdditionalInfo(330, compoundData, i, bondPositions, t)
                 t.forward(30)
                 t.left(60)
+                t.pencolor("black")
 
 if __name__ == "__main__":
     myGUI = GUI()
--- formula.py
+++ formula.py
@@ -1,64 +1,94 @@
 import re
 
-class FormulaConversion:
-    def __init__(self) -> None:
-        self.unbranchedAlkanes = ["methan", "ethan", "propan", "butan", "pentan", "hexan", "heptan", "oktan", "nonan", "dekan"]
-        self.preffixList = ["di", "tri", "tetra", "penta", "hexa"]
+unbranchedAlkanes = ["methan", "ethan", "propan", "butan", "pentan", "hexan", "heptan", "oktan", "nonan", "dekan"]
+preffixList = ["di", "tri", "tetra", "penta", "hexa"]
+suffixList = ["an", "en", "yn"]
+regexList = [r"\d-en", r"\d-yn"] # I dont like this solution so it would be good to change it
 
-    def inputFormat(self, inputString : str) -> list:
-        compoundFirstSplit = inputString.split("-") 
-        returnList = []
-        iterationSkip = 100 # This is so that i can skip over an iteration if the list element has been changed
+def inputFormat(inputString : str):
+    bondPositions = []
+    compoundFirstSplit = inputString.split("-")
+    returnList = []
+    iterationSkip = 100 # This is so that i can skip over an iteration if the list element has been changed
+    for i in range(0, len(compoundFirstSplit)): # This goes over the string and splits everything 
+        if(iterationSkip == i):
+            continue
+        elif(not re.search('[a-zA-Z]', compoundFirstSplit[i])):
+            returnList.append("-".join(compoundFirstSplit[i:i+2]))
+            iterationSkip = i + 1
+        else:
+            returnList.append(compoundFirstSplit[i])
+ 
+    for i in range(0, len(returnList)): # This checks for the num of bonds
+        for preffix in preffixList: # Removes preffix
+            if(preffix in returnList[i]):
+                returnList[i] = returnList[i].replace(preffix, "")
+                if(i == len(returnList) - 1): # If there is a preffix then remove the last char from previous element
+                    returnList[i-1] = returnList[i-1][:-1] 
 
-        for i in range(0, len(compoundFirstSplit)):
-            if(iterationSkip == i):
-                continue
-            elif(not re.search('[a-zA-Z]', compoundFirstSplit[i])):
-                returnList.append("-".join(compoundFirstSplit[i:i+2]))
-                iterationSkip = i + 1
-            else:
-                returnList.append(compoundFirstSplit[i])
+        for index in range(0, len(regexList)):
+            patternCheck = re.compile(regexList[index])
+            if(bool(patternCheck.search(returnList[i])) == True):
+                bondPositions = re.findall(r'[0-9]+', returnList[i]) # Finds nums
+                if(index == 0):
+                    bondPositions.insert(0, 2)
+                else:
+                    bondPositions.insert(0, 3)
+                returnList[i] = re.sub(r'[^a-zA-Z]', '', returnList[i])
+                # Adds an to the end of the string
+                for part in returnList:
+                    for suffix in suffixList:
+                        if(suffix == part):
+                            part = suffixList[0]
+                            returnList[i-1] += part 
+    
+    returnList = list(filter(lambda a: len(a) != 2, returnList)) # Removes all strings that have the len 2, assuming only the suffix stays
+    return returnList, bondPositions
 
-        return returnList
+def extractValues(listElement : str, residuesList : list) -> None: # Void that changes the list
+    positions = re.findall(r'[0-9]+', listElement) # Finds nums
+    listElement = re.sub(r'[^a-zA-Z]', '', listElement) # Removes all special chars and nums
+    for preffix in preffixList:
+        listElement = listElement.replace(preffix, "")
+    for i in range(0, len(unbranchedAlkanes)): # Goes through pre-defined list
+        if(unbranchedAlkanes[i].replace("an", "yl") == listElement): # Checks if listElement is in this predefined list
+            for index in positions: 
+                if(residuesList[int(index) - 1] == i + 1): # This indexing works and at the time of writing this code i dont want to re-check it
+                    residuesList[int(index) - 1] = [residuesList[int(index) - 1]] # Converts list variable into a list 
+                    residuesList[int(index) - 1].append(i + 1)
+                elif(type(residuesList[int(index) - 1]) is list): # If there is already a list there it just adds another element of that type
+                    residuesList[int(index) - 1].append(i + 1)
+                else:
+                    residuesList[int(index) - 1] = i + 1 # Regex returns nums as strings
 
-    def extractValues(self, listElement : str, residuesList : list) -> None: # Void that changes the list
-        positions = re.findall(r'[0-9]+', listElement) # Finds nums
-        listElement = re.sub(r'[^a-zA-Z]', '', listElement) # Removes all special chars and nums
-        for preffix in self.preffixList:
-            listElement = listElement.replace(preffix, "")
+def bondFormat(bondList : list, backboneLength : int) -> list:
+    bondsPositions = [0] * backboneLength
+    for i in range(1, len(bondList)):
+        bondsPositions[int(bondList[i]) - 1] = int(bondList[0])
+    return bondsPositions
 
-        for i in range(0, len(self.unbranchedAlkanes)): # Goes through pre-defined list
-            if(self.unbranchedAlkanes[i].replace("an", "yl") == listElement): # Checks if listElement is in this predefined list
-                for index in positions: 
-                    if(residuesList[int(index) - 1] == i + 1): # This indexing works and at the time of writing this code i dont want to re-check it
-                        residuesList[int(index) - 1] = [residuesList[int(index) - 1]] # Converts list variable into a list 
-                        residuesList[int(index) - 1].append(i + 1)
-                    elif(type(residuesList[int(index) - 1]) is list): # If there is already a list there it just adds another element of that type
-                        residuesList[int(index) - 1].append(i + 1)
-                    else:
-                        residuesList[int(index) - 1] = i + 1 # Regex returns nums as strings
+def mainFormula(inputString : str):
+    backboneLength = 0
+    compound, bondList = inputFormat(inputString)
+    compound = compound[::-1] # Reverse the list because its done the opposite way
+    for i in range(0, len(unbranchedAlkanes)): # First find the number of carbon on the backbone
+        if(compound[0] == unbranchedAlkanes[i]):
+            backboneLength = i + 1
+            break
 
-    def mainFormula(self, inputString : str):
-        backboneLength = 0
+    hydrocarbonResidues = [0] * backboneLength
+    bondPositions = bondFormat(bondList, backboneLength)
 
-        compound = self.inputFormat(inputString)[::-1] # Reverse the list because its done the opposite way
+    compound.pop(0) # Removes the part of the compound that determines the backbone length
+    for element in compound:
+        extractValues(element, hydrocarbonResidues)
 
-        for i in range(0, len(self.unbranchedAlkanes)): # First find the number of carbon on the backbone
-            if(compound[0] == self.unbranchedAlkanes[i]):
-                backboneLength = i + 1
-                break
-            
-        hydrocarbonResidues = [0] * backboneLength
-        compound.pop(0) # Removes the part of the compound that determines the backbone length
-
-        for element in compound:
-            self.extractValues(element, hydrocarbonResidues)
-
-        return hydrocarbonResidues
+    return hydrocarbonResidues, bondPositions
 
 if __name__ == "__main__":
+    testString = "2,3,3,7-tetrameth-2-yl-okt-1,2-en"
+    testString = "4-propyl-deka-2,4-dien" 
     #testString = "4-ethyl-3,5-dimethyl-oktan"
-    testString = "7-ethyl-2,2,6-trimethyl-5-propyl-dekan"
-    testString = "2,3,7-trimethyl-nonan"
-    conversion = FormulaConversion()
-    print(conversion.mainFormula(testString))
+    #print(inputFormat(testString))
+    #print(findMultipleBonds(inputFormat(testString)))
+    print(mainFormula(testString))
